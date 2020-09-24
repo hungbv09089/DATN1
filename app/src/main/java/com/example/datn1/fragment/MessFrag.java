f800077b3c7653554d1bede80470418e80f5cdd5
hungbv09089 <50956296+hungbv09089@users.noreply.github.com>
2020-09-24 21:12:07 +0700

    login

--- app/src/main/java/com/example/datn1/fragment/MessFrag.java
+++ app/src/main/java/com/example/datn1/fragment/MessFrag.java
@@ -1,29 +1,15 @@
 package com.example.datn1.fragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
 import com.example.datn1.R;
 
 public class MessFrag extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MessFrag() {
-        // Required empty public constructor
-    }
 
 
     @Override
